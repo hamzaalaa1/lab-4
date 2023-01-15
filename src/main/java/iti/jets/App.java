514b77a7b9616e84f70814bcaab10e2f7fdd079b
Hassan_7740 <hassan.lshrif.2011@gmail.com>
2023-01-15 19:41:39 +0200

    second update statment with JDQL

--- src/main/java/iti/jets/App.java
+++ src/main/java/iti/jets/App.java
@@ -11,7 +11,8 @@ public class App
     public static void main( String[] args )
     {
         EmpDBC_Q instance = new EmpDBC_Q();
-        List<Employees> resultList =  instance.findByEmployeeName("Parto");
-        resultList.stream().forEach((Employee)-> instance.printResult(Employee));
+        // List<Employees> resultList =  instance.findByEmployeeName("Parto");
+        // resultList.stream().forEach((Employee)-> instance.printResult(Employee));
+        instance.updateByEmployeeId(47056, "HASSAN");
     }
 }
--- src/main/java/iti/jets/EmpDBC_Q.java
+++ src/main/java/iti/jets/EmpDBC_Q.java
@@ -1,7 +1,5 @@
 package iti.jets;
-
 import java.util.*;
-
 import jakarta.persistence.*;
 
 public class EmpDBC_Q {
@@ -13,6 +11,15 @@ public class EmpDBC_Q {
             .setParameter("firstName", firstName);
       return findByEmployeeNAme.getResultList();
    }
+
+   public void updateByEmployeeId(int id , String Name){
+      Query findByEmployeeNAme = em.createQuery("update Employees e SET e.firstName =  ?1 WHERE e.empNo =  ?2")
+      .setParameter(1, Name).setParameter( 2 , id);
+      em.getTransaction().begin();
+      findByEmployeeNAme.executeUpdate();
+      em.getTransaction().commit();
+   }
+
 
    public void printResult(Employees s) {
       System.out.println("Employee name : " + s.getFirstName() + " " + "Employee id :" + s.getEmpNo());
